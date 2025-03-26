4dabe9d90b6ed0ca8ed0735c4db5419481dc1a6a
laroccacharly <larocca.charly@gmail.com>
2025-03-26 15:14:02 -0400

    update commands

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # DOCKER_DEFAULT_PLATFORM=linux/amd64 
-alias build="DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build api producer broker consumer"
-alias up="docker compose up api producer broker consumer"
+alias build="DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build"
+alias up="docker compose up"
 alias k="uv run cli.py"
 alias restartc="docker compose restart consumer"
