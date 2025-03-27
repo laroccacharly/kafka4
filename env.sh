dad43fe1293d02035be5b85de0561eb3e9be233f
laroccacharly <larocca.charly@gmail.com>
2025-03-27 10:32:31 -0400

    add down command

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 # DOCKER_DEFAULT_PLATFORM=linux/amd64 
 alias build="DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build"
 alias up="docker compose up"
+alias down="docker compose down"
 alias k="uv run cli.py"
 alias restartc="docker compose restart consumer"
 alias restartp="docker compose restart producer"
